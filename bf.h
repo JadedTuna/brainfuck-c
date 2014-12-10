f120d9a72f45a11cd753b4cf2ccf9315c33811cf
Victor <digitloft@gmail.com>
2014-12-10 21:37:48 +0100

    Update bf.h
    Changed bracemap and memory types' to `int` from `char`

--- bf.h
+++ bf.h
@@ -1,7 +1,7 @@
 #define MEMSIZE  65535
 #define TAPESIZE 30000
-unsigned char memory[MEMSIZE];
-unsigned char bracemap[MEMSIZE];
+unsigned int memory[MEMSIZE];
+unsigned int bracemap[MEMSIZE];
 unsigned int temp_stack[MEMSIZE];
 unsigned char tape[TAPESIZE];
 unsigned int pointer, pc, sp;
